99d27f0cf6abb6d7cce98090e4a2272cabea49cd
SiRipo92 <ripochesierra@gmail.com>
2024-12-05 11:46:03 -0500

    Task 3: Format/parse the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     """Function to analyze emotion from text using the Watson NLP library.
@@ -16,5 +17,24 @@ def emotion_detector(text_to_analyse):
     # Send POST request to the API with text
     response = requests.post(url, json = myobj, headers=headers ) # Send POST request to the API w/ text
 
-    # Return the text attribute of the response object
-    return response.text
+    # Parse response text into a dictionary
+    formatted_response = json.loads(response.text)
+
+    # Extract emotion scores
+    # ["emotionPredictions"] accesses the top-level key in the dict containing the list of predictions
+    # [0] selects the 1st element of the list, being emotions, which is a dictionary of emotions
+    # ["emotions"] accesses the emotion dict inside the 1st prediction where indv. emotion scores are stored
+    emotions = formatted_response["emotionPredictions"][0]["emotion"]
+
+    # Find the dominant emotion (the one with the highest score)
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    # Return the emotions as a dictionary with their corresponding scores and dominant emotion
+    return {
+        "anger": emotions["anger"],
+        "disgust": emotions["disgust"],
+        "fear": emotions["fear"],
+        "joy": emotions["joy"],
+        "sadness": emotions["sadness"],
+        "dominant_emotion": dominant_emotion,
+    }
